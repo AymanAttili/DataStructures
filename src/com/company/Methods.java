195fadac072093862ff1ce9ced9f45ff97cb6a75
AymanAttili <96499629+AymanAttili@users.noreply.github.com>
2022-01-08 22:05:30 +0200

    wow

--- src/com/company/Methods.java
+++ src/com/company/Methods.java
@@ -5,7 +5,7 @@ import sun.swing.MenuItemLayoutHelper;
 import java.util.*;
 import java.lang.*;
 
-public class Main {
+public class Methods {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
